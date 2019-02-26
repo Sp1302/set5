00be8ca2feb22111c8ac7a69633a281ac9cc90d7
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 11:38:12 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+# set5
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nenter the number");
+scanf("%d\n",&n);
+if((n>1)&&(n<10))
+{
+printf("%d\n",n);
+}
+else
+{
+printf("\nnot in range");
+}
+return 0;
+}
