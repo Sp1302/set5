ea67c9ba1d6be8661845b9151948dfaa6b2354df
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 11:52:43 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,17 @@
+# set5
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nenter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n %d is power of 2",n);
+}
+else
+{
+printf("not a power of two\n");
+}
+return 0;
+}
